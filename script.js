c5d8ebd1f425cfb30fc38d42346bc510a662caa8
vali2244 <syedvali221@gmail.com>
2023-07-23 13:50:49 +0530

    ok

---
+++ script.js
@@ -0,0 +1,25 @@
+// two methode of arrays declear
+// let x=[11,22,55,77,66,99]
+// x[6]=88
+// console.log(x.length)
+// let y= new Array (11,44,77,88,90,55)
+// console.log(y[0])
+// methods of array
+// let m=[22,44,66,88,99,44]
+// m.push(11)
+// console.log(m)
+// let y=[88,22,40,60,22]
+// y.pop(22)
+// console.log(y)
+// let n=[22,66,77,55]
+// n.unshift(88)
+// console.log(n)
+// let p=[44,77,7,66,99,22]
+// p.shift()
+// console.log(p)
+// let h=[44,66.77,22,33,'vali']
+
+// console.log("index of vali:"+h.indexOf("vali"))
+// let t=[33,66,77,44,5,2,99]
+// t.slice(2,5)
+// console.log(t.slice(2,5))
